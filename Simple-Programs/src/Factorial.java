a90793650f164b283dafe32c0af43abac39f76e1
Shreya Kapoor <shreyakapoor98@gmail.com>
2020-10-02 12:36:55 +0530

    Calculate Factorial

---
+++ Simple-Programs/src/Factorial.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class Factorial {
+
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+        Scanner sc = new Scanner(System.in);
+        int[] num = new int[5];
+        for(int i=0;i<5;i++) {
+            num[i] = sc.nextInt();
+            long res = factorial(num[i]);
+            System.out.println(res);
+        }
+        sc.close();
+
+    }
+    public static long factorial(int n) {
+        long fact=1;
+        for(int i=1;i<=n;i++) {
+            fact = fact*i;
+        }
+        return fact;
+        
+    }
+
+}
+
+
