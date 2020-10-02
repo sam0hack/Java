25524f90131b5a3848aa8e46731175a723cca806
Shreya Kapoor <shreyakapoor98@gmail.com>
2020-10-02 12:46:42 +0530

    Comments added

--- Simple-Programs/src/Factorial.java
+++ Simple-Programs/src/Factorial.java
@@ -1,15 +1,17 @@
-import java.util.Scanner;
+//program to calculate factorial.
+
+import java.util.Scanner; //package included for Scanner
 
 public class Factorial {
 
     public static void main(String[] args) {
         // TODO Auto-generated method stub
-        Scanner sc = new Scanner(System.in);
+        Scanner sc = new Scanner(System.in); //taking input
         int[] num = new int[5];
         for(int i=0;i<5;i++) {
             num[i] = sc.nextInt();
             long res = factorial(num[i]);
-            System.out.println(res);
+            System.out.println(res); //printing result
         }
         sc.close();
 
@@ -17,7 +19,7 @@ public class Factorial {
     public static long factorial(int n) {
         long fact=1;
         for(int i=1;i<=n;i++) {
-            fact = fact*i;
+            fact = fact*i; //calculating factorial
         }
         return fact;
         
